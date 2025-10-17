f5d2fb47098bb4f362afd4a193556779aa2603a3
Ignatius <seeban1712@gmail.com>
2025-10-17 18:07:04 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Hi",
-    image: "./avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "An Ordinary Joke",
     email: "",
