7c0e3026da29d7b18d3413fd10feb11a0c78e405
Ignatius <seeban1712@gmail.com>
2025-10-20 13:59:25 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,3 +3,14 @@ module.exports = {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
 }
+
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+ 
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+ 
+export default nextConfig;
